1d7232a035c4942a845c4e5027c4e92e5b73f6bf
faizarachma <faizarachma@gmail.com>
2022-12-18 22:20:16 +0700

    update finalproject

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://final-project-42adf.web.app" // TODO: replace this
 const studentName = "Faiza Rachma" // TODO: replace this
 const studentId = "FE2878410" // TODO: replace this
 
